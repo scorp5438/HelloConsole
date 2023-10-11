3c173ca8078c26174fafd45cc6b173d1e4bfe8e9
ScOrP <alex_77_90@mail.ru>
2023-10-11 18:27:51 +0500

    Example002_HelloUser

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Введите ваше имя: "); 
+string username = Console.ReadLine(); // Присвоение введенной информации переменной username, черех команду ввода Console.ReadLine()
+Console.WriteLine("Привет," + " " + username);
