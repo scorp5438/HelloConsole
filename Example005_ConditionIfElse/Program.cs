b998ca649c42a7ed13b4c9d75face409ef879c41
ScOrP <alex_77_90@mail.ru>
2023-10-11 19:53:37 +0500

    Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите ваше имя: ");
+string name = Console.ReadLine();
+if (name.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша");  
+}
+else
+{
+    Console.WriteLine("Привет, " + name);
+}
+
