9d8d03cf9c749ade84ae9c6cb74a1d22d29bf164
ScOrP <alex_77_90@mail.ru>
2023-10-11 20:18:58 +0500

    Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 1, b = 2,
+    c = 6, d = 8,
+    e = 4;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine(max);
