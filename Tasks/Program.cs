343fba4ec616afb51d33beb4df64f6e318de758f
ScOrP <alex_77_90@mail.ru>
2023-10-19 23:55:41 +0500

    Tasks_v3

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -30,7 +30,7 @@
 
 // string WhoseMove(string lastPlayer, bool win)
 // {
-    
+
 //     if((lastPlayer == "white") && (win == true))
 //     {
 //         lastPlayer = "white";
@@ -78,11 +78,208 @@
 //     Console.WriteLine(false);
 // }
 
-int a = 798;
+// int a = 798;
 
-int z = (int)Char.GetNumericValue(a.ToString()[1]);
+// int z = (int)Char.GetNumericValue(a.ToString()[1]);
 
-int r = int.Parse(a.ToString()[1].ToString());
+// int r = int.Parse(a.ToString()[1].ToString());
 
-Console.WriteLine($"Тип данных переменной z равной {z}: {z.GetTypeCode()}");
-Console.WriteLine($"Тип данных переменной z равной {r}: {r.GetTypeCode()}");
+// Console.WriteLine($"Тип данных переменной z равной {z}: {z.GetTypeCode()}");
+// Console.WriteLine($"Тип данных переменной z равной {r}: {r.GetTypeCode()}");
+
+// int p = (a / 10) % 10;
+
+// Console.WriteLine(p);
+
+// int temp  = 49;
+
+
+// if(temp % 10 > temp / 10)
+// {
+//     Console.WriteLine(temp % 10);
+// }
+// else
+//     Console.WriteLine(temp / 10);
+
+
+// Reversed Strings
+
+// string str = "World";
+// string res = "";
+// for (int i = str.Count() - 1; i > - 1; i -= 1)
+// {
+//     res += str[i];
+// }
+// Console.WriteLine(res);
+
+// Remove First and Last Characte
+
+// string str = "ofgodfgdk";
+// int i = 0;
+// string res = "";
+// while(i < str.Length)
+// {
+//     if (i != 0 && i != str.Length - 1)
+//     {
+//         res += str[i];
+//     }
+//     i++;
+// } 
+// Console.WriteLine(res);
+
+
+// Square(n) Sum
+
+// int[] array = { 1, 2, 2 };
+// int res = 0;
+// foreach(int i in array)
+// {
+//     res += i * i;
+// }
+// Console.WriteLine(res);
+
+
+// String repeat
+
+// string s = ("#");
+// int n = 5;
+// int i = 1;
+// string res = "";
+// while(i <= n)
+// {
+//     res = res + s;
+//     i++;
+// }
+
+
+// // string res = string.Concat(Enumerable.Repeat(s , n));
+// Console.WriteLine(res);
+
+
+// Grasshopper - Summation
+
+// int num = 100;
+// int i = 1;
+// int res = 0;
+// while(i <= num)
+// {
+//     res += i;
+//     i++;
+// }
+// Console.WriteLine(res);
+
+
+// Remove String Spaces
+
+// string input = "апвап вава  ва в ва ";
+
+// string res = input.Replace(" ","");
+
+// Console.WriteLine(res);
+
+
+// Counting sheep...
+
+// var sheeps = new bool[] { true, false, true, false, true, false, true, false, true, false, true };
+
+// int i = 0;
+// int res = 0;
+// while(i < sheeps.Length)
+// {
+//     if(sheeps[i])
+//     {
+//         res++;
+//     }
+//     i++;
+// }
+// Console.WriteLine(res);
+
+
+// Basic Mathematical Operations
+
+// char operation = '/';
+// double value1 = 49;
+// double value2 = 7;
+// double res = 0;
+// if(operation == '+')
+// {
+//     res = value1 + value2;
+// }
+// if(operation == '-')
+// {
+//     res = value1 - value2;
+// }
+// if(operation == '*')
+// {
+//     res = value1 * value2;
+// }
+// if(operation == '/')
+// {
+//     res = value1 / value2;
+// }
+// Console.WriteLine(res);
+
+
+// Keep Hydrated!
+
+// double time = 6.7;
+
+// double temp = time * 0.5;
+
+// Console.WriteLine(Convert.ToInt32(Math.Floor(temp)));
+
+
+// Century From Year
+
+// int year = 1801;
+// if(year % 100 == 0)
+// {
+//     Console.WriteLine(year / 100);
+// }
+// else
+// {
+//     Console.WriteLine(year / 100 + 1);
+// }
+
+
+// Abbreviate a Two Word Name
+
+// string name = "patrick feenan";
+
+// // string res = name.ToUpper()[0] + "." + name.ToUpper()[name.IndexOf(" ") + 1];
+
+// string[] temp = name.Split(new char[] {' '});
+// string res = string.Join('.', temp[0][0], temp[1][0]).ToUpper();
+
+// Console.WriteLine(res);
+
+
+// Convert number to reversed array of digits
+
+// int n = 35231;
+
+// string str = n.ToString();
+
+// long[] numbers = new long[str.Length];
+// int index = 0;
+// for (int i = str.Length - 1; i > -1; i -= 1)
+// {
+//     numbers[index] = Convert.ToUInt32(str[i].ToString());
+//     index += 1;
+// }
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     Console.Write($"{numbers[i]} ");
+// }
+    
+// A Needle in the Haystack
+
+// string[] arr = {"hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"};
+// int res= Array.IndexOf(arr, "needle");
+// Console.WriteLine(res);
+
+// double[] array = { 17, 16, 16, 16, 16, 15, 17, 17, 15, 5, 17, 17, 16 };
+
+// double res = array.Sum();
+
+// Console.WriteLine(array.Average());
