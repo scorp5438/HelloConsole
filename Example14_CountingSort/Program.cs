b574aa652b789d8f3cc148e343aafe6434159904
ScOrP <alex_77_90@mail.ru>
2023-11-26 13:11:25 +0500

    Example14

---
+++ Example14_CountingSort/Program.cs
@@ -0,0 +1,54 @@
+﻿// Сортировка подсчетом
+
+int[] array = { 1001, 100, 1001, 102};
+
+Console.WriteLine($"[{String.Join(", ", array)}]\n");
+
+// void CountingSort(int[] inputArray)
+// {
+//     int[] counters = new int[10];
+//     for (int i = 0; i < inputArray.Length; i++)
+//     {
+//         counters[inputArray[i]]++;
+//         // int ourNumber = inputArray[i];
+//         // counters[ourNumber]++;
+//     }
+//     int index = 0;
+//     for (int i = 0; i < counters.Length; i++)
+//     {
+//         for (int j = 0; j < counters[i]; j++)
+//         {
+//             inputArray[index] = i;
+//             index++;
+//         }
+//     }
+// }
+
+void CountingSortExtended(int[] inputArray)
+{
+    int max = inputArray.Max();
+    int min = inputArray.Min();
+
+    int offset = -min;
+    int[] counters = new int[max + offset + 1];
+    
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i] + offset]++;
+        // int ourNumber = inputArray[i];
+        // counters[ourNumber]++;
+    }
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            inputArray[index] = i - offset;
+            index++;
+        }
+    }
+}
+
+CountingSortExtended(array);
+
+Console.WriteLine($"[{String.Join(", ", array)}]");
